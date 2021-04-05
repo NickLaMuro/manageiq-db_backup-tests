bf9a0b7cf1804229969591363485fe81c9fbca68
Nick LaMuro <nicklamuro@gmail.com>
2021-04-05 11:07:49 -0500

    [DbFilenameHelper] Parse based on `.basename`
    Ensures that nothing in the file path is parsed when looking at the
    filename.

--- test/support/db_filename_helper.rb
+++ test/support/db_filename_helper.rb
@@ -1,10 +1,11 @@
 module DbFilenameHelper
   def parse_db_filename filename
-    parts    = filename.split("_") - ["console"]
+    base     = File.basename filename
+    parts    = base.split("_") - ["console"]
     split    = parts.first == "split"
     location = parts[1].to_sym
     type     = parts.last.split(".").first
-    console  = filename.start_with?("console")
+    console  = base.start_with?("console")
 
     [location, type, console, split]
   end
