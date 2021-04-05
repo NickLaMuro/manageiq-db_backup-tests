fe4dc198566bae2cf56843f6ebabf670e9235282
Nick LaMuro <nicklamuro@gmail.com>
2021-04-05 11:05:07 -0500

    [test/{nfs|smb}*.rb] Fix NFS/SMB tests
    - `require`/`include` `MountHelper`
    - Uses proper assertions
    - Uses proper names for DB files (for DbFilenameHelper)
    - Adds teardown (just cleans up ApplianceConsoleRunner::Current)

--- test/nfs_backup_test.rb
+++ test/nfs_backup_test.rb
@@ -1,6 +1,9 @@
 require "test_helper"
 
+require_relative "./support/mount_helper.rb"
+
 class NFSBackupTest < BaseBackupTest
+  include MountHelper
 
   def test_nfs_database_backup
     console_nfs_backup_file = "console_full_nfs_backup.tar.gz"
@@ -8,11 +11,10 @@ class NFSBackupTest < BaseBackupTest
 
     run_in_mount :nfs do |mount_point|
       mount_file = File.join(mount_point, "db_backup", console_nfs_backup_file)
-      assert_file_exists mount_file
-      original_console_nfs_backup_size = get_file_size mount_file
-    end
 
-    assert_valid_database console_nfs_backup_file
+      assert_file_exists mount_file
+      assert_valid_database mount_file
+    end
   end
 
   def test_nfs_database_dump
@@ -22,20 +24,25 @@ class NFSBackupTest < BaseBackupTest
 
     run_in_mount :nfs do |mount_point|
       mount_file = File.join(mount_point, "db_dump", console_nfs_dump_file)
-      assert_file_exists mount_file
-    end
 
-    assert_valid_database console_nfs_dump_file,
+      assert_file_exists mount_file
+      assert_valid_database mount_file
+    end
   end
 
   def test_nfs_database_dump_excluding_custom_attributes
-    console_nfs_dump_file_without_ca = "console_nfs_dump_without_custom_attributes.tar.gz"
+    console_nfs_dump_file_without_ca = "console_partial_nfs_dump_without_custom_attributes.tar.gz"
     ApplianceConsoleRunner.dump_with_no_custom_attributes console_nfs_dump_file_without_ca, :nfs
 
     run_in_mount :nfs do |mount_point|
       mount_file = File.join(mount_point, "db_dump", console_nfs_dump_file_without_ca)
+
       assert_file_exists mount_file
-      assert_valid_database console_nfs_dump_file_without_ca
+      refute_has_custom_attributes mount_file
     end
+  end
+
+  def teardown
+    ApplianceConsoleRunner::Current.instance = nil
   end
 end
--- test/smb_backup_test.rb
+++ test/smb_backup_test.rb
@@ -1,6 +1,9 @@
 require "test_helper"
 
+require_relative "./support/mount_helper.rb"
+
 class SMBBackupTest < BaseBackupTest
+  include MountHelper
 
   def test_smb_database_backup
     console_smb_backup_file = "console_full_smb_backup.tar.gz"
@@ -8,11 +11,10 @@ class SMBBackupTest < BaseBackupTest
 
     run_in_mount :smb do |mount_point|
       mount_file = File.join(mount_point, "db_backup", console_smb_backup_file)
-      assert_file_exists mount_file
-      original_console_smb_backup_size = get_file_size mount_file
-    end
 
-    assert_valid_database console_smb_backup_file
+      assert_file_exists mount_file
+      assert_valid_database mount_file
+    end
   end
 
   def test_smb_database_dump
@@ -22,20 +24,25 @@ class SMBBackupTest < BaseBackupTest
 
     run_in_mount :smb do |mount_point|
       mount_file = File.join(mount_point, "db_dump", console_smb_dump_file)
-      assert_file_exists mount_file
-    end
 
-    assert_valid_database console_smb_dump_file,
+      assert_file_exists mount_file
+      assert_valid_database mount_file
+    end
   end
 
   def test_smb_database_dump_excluding_custom_attributes
-    console_smb_dump_file_without_ca = "console_smb_dump_without_custom_attributes.tar.gz"
+    console_smb_dump_file_without_ca = "console_partial_smb_dump_without_custom_attributes.tar.gz"
     ApplianceConsoleRunner.dump_with_no_custom_attributes console_smb_dump_file_without_ca, :smb
 
     run_in_mount :smb do |mount_point|
       mount_file = File.join(mount_point, "db_dump", console_smb_dump_file_without_ca)
+
       assert_file_exists mount_file
-      assert_valid_database console_smb_dump_file_without_ca
+      refute_has_custom_attributes mount_file
     end
+  end
+
+  def teardown
+    ApplianceConsoleRunner::Current.instance = nil
   end
 end
